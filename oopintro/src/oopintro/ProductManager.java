a849bf892fae753f202448446e21fb8fc9709aca
Tuğçe Saygın <tugcesaygin.05@gmail.com>
2021-04-25 00:55:38 +0300

    intro added

---
+++ oopintro/src/oopintro/ProductManager.java
@@ -0,0 +1,11 @@
+package oopintro;
+
+public class ProductManager { // Producti ilgilendiren metotlari bunu altina yaziyoruz
+
+	public void addToCart(Product product) {  //metot - ikinci product gonderiliyo
+		
+		System.out.println("Sepete ekleme kodu acildi! " + product.name);
+		
+	}
+	
+}
