4887be97f7ff0e556170a7e0cd876cf528f95717
Tuğçe Saygın <79403885+tugcesaygin@users.noreply.github.com>
2021-05-06 01:10:12 +0300

    Update and rename ICampainService.java to ICampaignService.java

---
+++ L4GameProject/src/Abstract/ICampaignService.java
@@ -0,0 +1,16 @@
+package Abstract;
+
+import Entites.Campaign;
+
+public interface ICampaignService {
+
+	
+
+	void addNewCampaign(Campaign campaign);
+	
+	void deleteCampaign(Campaign campaign);
+	
+	void updateCampaign(Campaign campaign);
+	
+	
+}
