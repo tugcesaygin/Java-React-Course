2840c8ee697abecf518a01841dcf85e3c5510ddf
Tuğçe Saygın <79403885+tugcesaygin@users.noreply.github.com>
2021-05-06 01:08:47 +0300

    Update SaleManager.java

--- L4GameProject/src/Concrete/SaleManager.java
+++ L4GameProject/src/Concrete/SaleManager.java
@@ -1,18 +1,18 @@
 package Concrete;
 
 import Abstract.ISaleService;
-import Entites.Campain;
+import Entites.Campaign;
 import Entites.Game;
 import Entites.Gamer;
 
 public class SaleManager implements ISaleService{
 
 	@Override
-	public void sale(Gamer gamer, Game game, Campain campain) {
-		double discountedPrice = game.getPrice() - (game.getPrice()* campain.getDiscountRate()/100);
+	public void sale(Gamer gamer, Game game, Campaign campaign) {
+		double discountedPrice = game.getPrice() - (game.getPrice()* campaign.getDiscountRate()/100);
 		
 		System.out.println(gamer.getFirstName() +" " + gamer.getLastName()
-		+"bought " + game.getGameName() + "for £" + discountedPrice + "with a discount called" +campain.getCampainName() );
+		+"bought " + game.getGameName() + "for Â£" + discountedPrice + "with a discount called" +campaign.getCampaignName() );
 		
 	}
 
