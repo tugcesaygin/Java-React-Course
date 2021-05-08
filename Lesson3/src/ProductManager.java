9847618592790c984128fbb0edd4357c4fd3a7be
Tuğçe Saygın <79403885+tugcesaygin@users.noreply.github.com>
2021-05-08 23:31:04 +0300

    Delete Lesson3 directory

--- Lesson3/src/ProductManager.java
+++
@@ -1,8 +0,0 @@
-
-public class ProductManager {
-	
-	public void addToCart(Product product) { // metot - ikinci product gonderiliyo
-
-		System.out.println("Sepete ekleme kodu acildi! " + product.getName());
-	}
-}
